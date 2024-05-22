d503fe58c81a4908b62abe04b39fa1d17038c2f0
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2024-05-22 17:34:57 +0200

    Added favicon

--- site.hs
+++ site.hs
@@ -32,6 +32,10 @@ main = hakyll $ do
         route idRoute
         compile copyFileCompiler
 
+    match "favicon.ico" $ do
+        route idRoute
+        compile copyFileCompiler
+
     match "*.md" $ do
         route $ setExtension "html"
         compile $ do
